4c63db6cc5789119fbc93c97b924c2f5bb9f354b
Samad10jan <abdulsamad10jan@gmail.com>
2025-08-19 19:47:03 +0530

    vercerl commit 12

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -36,7 +36,7 @@ export default function Login() {
                 console.log("Login Success");
                 
 
-               
+               window.location.href = "/"; // Redirect to home page
                 router.push("/")
                 setLoading(true)
 
