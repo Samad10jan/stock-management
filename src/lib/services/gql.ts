aa828dbe05734551a6bb19956f1c62edcec1ab57
Samad10jan <abdulsamad10jan@gmail.com>
2025-08-19 18:05:57 +0530

    vercerl commit 10

--- src/lib/services/gql.ts
+++ src/lib/services/gql.ts
@@ -1,5 +1,5 @@
 import { gql, GraphQLClient } from "graphql-request";
 
-const gqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_HOST_URL+"/graphql");
+const gqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_HOST_URL+"/api/graphql");
 
 export default gqlClient;
