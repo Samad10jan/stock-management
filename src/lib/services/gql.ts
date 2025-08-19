bd47fddee320f383a266b6aa086f59e05f69a547
Samad10jan <abdulsamad10jan@gmail.com>
2025-08-19 17:57:17 +0530

    vercerl commit 9

--- src/lib/services/gql.ts
+++ src/lib/services/gql.ts
@@ -1,4 +1,10 @@
 import { gql, GraphQLClient } from "graphql-request";
 
-const gqlClient = new GraphQLClient("https://stock-management-sigma-sepia.vercel.app/api/graphql");
+const gqlClient = new GraphQLClient("https://stock-management-sigma-sepia.vercel.app/api/graphql", {
+  headers: {
+    
+  },
+  credentials: "include", // This helps send cookies if needed
+});
+
 export default gqlClient;
