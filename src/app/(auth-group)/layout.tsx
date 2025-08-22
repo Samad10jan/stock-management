130c43b94d5111699436d4dc6046024ebb8fb894
Samad10jan <abdulsamad10jan@gmail.com>
2025-08-22 11:30:18 +0530

    Issue remain unsolved

--- src/app/(auth-group)/layout.tsx
+++ src/app/(auth-group)/layout.tsx
@@ -5,6 +5,8 @@ import Head from "next/head";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
+export const dynamic = "force-dynamic";
+
 export default async function Layout({ children }: {
     children: ReactNode
 }) {
