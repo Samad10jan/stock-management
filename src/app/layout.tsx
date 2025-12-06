42b8ad906933f143649f17931513e5df6e17c26e
Samad10jan <abdulsamad10jan@gmail.com>
2025-12-06 21:59:59 +0530

    -chnaged next version next@15.4.8

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/components/contexts/theme-context";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Stock Manager",
@@ -26,9 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <ThemeProvider>
 
         {children}
